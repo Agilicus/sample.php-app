c5e4de5df71ec56d97457f08b07a8673f0428d65
Don Bowman <don@agilicus.com>
2020-09-22 11:17:51 -0400

    fix: use relative redirect uri

--- src/index.php
+++ src/index.php
@@ -3,7 +3,8 @@ session_start();
 
 $client_id = 'sample-php-app';
 $client_secret = 'NOTREALLYSECRET';
-$redirect_uri = 'http://localhost:4200/';
+// $redirect_uri = 'http://localhost:4200/';
+$redirect_uri = url_origin($_SERVER);
 $metadata_url = 'https://auth.cloud.egov.city/.well-known/openid-configuration';
 
 
@@ -109,3 +110,14 @@ function http($url, $params=false, $headers=false) {
   return json_decode(curl_exec($ch));
 }
 
+function url_origin( $s )
+{
+    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+    $port     = $s['SERVER_PORT'];
+    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+    $host     = isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null;
+    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+    return $protocol . '://' . $host . '/';
+}
